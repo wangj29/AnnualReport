1c6d86a3d3a51f0f52002a040f6fca02de2e024b
wangj29 <wangj29@miamioh.edu>
2014-12-13 19:53:30 -0500

    a file has been deleted

--- application/views/committee/drop_member_dropdown.php
+++
@@ -1,6 +0,0 @@
-<?php
-$query=$this->committee_model->get_committee($committee);
-foreach($query->result() as $member){
-    echo '<option value="'.$member->member_name.'" >'.$member->member_name.'</option>';
-}
-?>
